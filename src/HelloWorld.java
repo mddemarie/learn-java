d8eda51d9876081e0301d22225aae1a1bce3ad16
mddemarie <mddemarie@gmail.com>
2018-08-13 20:39:41 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+This is a class HelloWorld. It prints the text Hello World and my name.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){ // this is a method
+        System.out.println("Hello world!"); // this is a statement
+        System.out.println("My name is Marie.");
+    }
+}
