2758ad4987c114c76fe842fc648678185dd8971c
mddemarie <mddemarie@gmail.com>
2018-08-13 22:19:13 +0200

    learnt about variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println("x is " + x);
+
+        x = 15;
+        System.out.println("x is now " + x);
+
+        int y;
+        y = x + 1;
+        System.out.println("x and y are " + x + " and " + y);
+    }
+}
